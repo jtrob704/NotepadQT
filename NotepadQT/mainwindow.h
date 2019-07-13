4602e11e23b736eef4ed1c72c3b520cfacf95a4e
J.T. Robinson <jaittarius@hotmail.com>
2019-07-12 23:41:24 -0400

    Fully functional build

--- NotepadQT/mainwindow.h
+++ NotepadQT/mainwindow.h
@@ -3,6 +3,13 @@
 
 #include <QMainWindow>
 
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +22,30 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_As_triggered();
+
+    void on_actionPrint_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString currentFile = "";
 };
 
 #endif // MAINWINDOW_H
